6e9811b57a06e19f628fb54e1d8a206f62690ecc
hutong <hutong236>
2018-06-21 23:46:59 +0800

    add my-information

--- test_project/mysite/account/urls.py
+++ test_project/mysite/account/urls.py
@@ -25,4 +25,11 @@ urlpatterns = [
     url(r'^register/$', views.register,name='user_register'),
     url(r'^password-change/$',auth_views.password_change,{'post_change_redirect':'/account/password-change-done'},name='password_change'),
     url(r'^password-change-done/$',auth_views.password_change_done,name='password_change_done'),
+    url(r'^password-reset/$',auth_views.password_reset,{"post_reset_redirect":'/account/password-reset-done',},name='password_reset'),
+    url(r'^password-reset-done/$',auth_views.password_reset_done,{'template_name':'registration/password_reset_done.html','post_change_redirect':'/account/password-reset-confirm'},name='password_reset_done'),
+    url(r'^password-reset-confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',auth_views.password_reset_confirm,{'template_name':'registration/password_reset_confirm.html','post_change_redirect':'/account/password-reset-complete'},name='password_reset_confirm'),
+    url(r'^password-reset-complete/$',auth_views.password_reset_complete,{'template_name':'registration/password_reset_complete.html'},name='password_reset_complete'),
+    url(r'^my-information/$',views.myself,name="my_information"),
+    url(r'^edit-my-information/$',views.myself_edit,name="edit_my_information"),
+
 ]
--- test_project/mysite/account/views.py
+++ test_project/mysite/account/views.py
@@ -1,8 +1,11 @@
 #coding:utf-8
 from django.shortcuts import render
-from .forms import LoginForm,RegistrationForm,UserProfileForm
-from django.http import HttpResponse
+from .forms import LoginForm,RegistrationForm,UserProfileForm,UserForm,UserInfoForm
+from .models import UserInfo,UserProfile
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -57,6 +60,8 @@ def register(request):
             new_profile = userprofile.save(commit=False)
             new_profile.user = new_user
             new_profile.save()
+            UserInfo.objects.create(user=new_user)
+
 
             return  HttpResponse("successfully")
         else:
@@ -67,4 +72,47 @@ def register(request):
         return render(request,"account/registration.html",{"form":user_form,"profile":userprofile_form})
 
 
-    #测试
+@login_required(login_url='/account/new-login')
+def myself(request):
+    user = User.objects.get(username=request.user.username)
+    userprofile = UserProfile.objects.get(user=user)
+    userinfo = UserInfo.objects.get(user=user)
+    print(userinfo.aboutme)
+    return render(request,"account/myself.html",{"user":user,"userinfo":userinfo,"userprofile":userprofile})
+
+
+@login_required(login_url='/account/new-login')
+def myself_edit(request):
+    user = User.objects.get(username=request.user.username)
+    userprofile = UserProfile.objects.get(user=user)
+    userinfo = UserInfo.objects.get(user=user)
+
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
+        userinfo_form = UserInfoForm(request.POST)
+
+        if user_form.is_valid() * userprofile_form.is_valid() * userinfo_form.is_valid():
+            user_cd = user_form.cleaned_data
+            userprofile_cd = userprofile_form.cleaned_data
+            userinfo_cd = userinfo_form.cleaned_data
+            print(user_cd["email"])
+            user.email = user_cd['email']
+            userprofile.birth = userprofile_cd['birth']
+            userprofile.phone = userprofile_cd['phone']
+            userinfo.school = userinfo_cd['school']
+            userinfo.profession = userinfo_cd['profession']
+            userinfo.company = userinfo_cd['company']
+            userinfo.address = userinfo_cd['address']
+            userinfo.aboutme = userinfo_cd['aboutme']
+            user.save()
+            userprofile.save()
+            userinfo.save()
+        return HttpResponseRedirect('/account/my-information/')
+    else:
+        user_form = UserForm(instance=request.user)
+        userprofile_form = UserProfileForm(initial={"birth":userprofile.birth,"phone":userprofile.phone})
+        userinfo_form = UserInfoForm(initial={"school":userinfo.school,"company":userinfo.company, \
+                                              "profession":userinfo.profession,"address":userinfo.address,"aboutme":userinfo.aboutme})
+
+        return render(request,"account/myself_edit.html",{"user_form":user_form,"userprofile_form":userprofile_form,"userinfo_form":userinfo_form})
